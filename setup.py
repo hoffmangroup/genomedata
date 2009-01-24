8af42950155fc84f891398baf6145a796e22f8b2
mmh1 <devnull@localhost>
2009-01-24 00:10:00 +0000

    prerequisite specification

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ dependency_links = ["http://pypi.python.org/packages/source/p/path.py/path-2.2.z
 classifiers = ["Natural Language :: English",
                "Programming Language :: Python"]
 
-install_requires = ["path", "tables>2.0.4"]
+install_requires = ["numpy", "path", "tables>2.0.4"]
 
 setup(name=name,
       version=__version__,
