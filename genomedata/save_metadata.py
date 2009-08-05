0d438ef30615aa9e1388bc9be1be69d4e444dc9e
mmh1 <devnull@localhost>
2009-08-05 22:11:35 +0000

    strip out seq_counts, seq_total_counts, etc. from metadata

--- genomedata/save_metadata.py
+++ genomedata/save_metadata.py
@@ -11,46 +11,16 @@ __version__ = "$Revision$"
 
 import sys
 
-from numpy import (amin, amax, append, array, diff, hstack, isfinite, NINF,
-                   PINF, square)
+from numpy import (amin, amax, array, diff, hstack, isfinite, NINF, PINF,
+                   square)
 from tables import openFile
 
 from .load_seq import MIN_GAP_LEN
 from ._util import (fill_array, get_tracknames, init_num_obs, new_extrema,
                     walk_continuous_supercontigs)
 
-
-def get_nucleotide_index (nuc, nucs):
-# Mirela: get the index of the specified nucleotide nuc in the array nucs
-    assert len(nucs) == 3    
-    if (nuc.tostring().upper() in ('A','T')):
-        return 0
-    if (nuc.tostring().upper() in ('C','G')):
-        return 1
-    return 2    
-                
-def get_dinucleotide_index (dinuc1, dinuc2, dinucs):
-    # XXX: this is REALLY not a good way of doing this. Switch to a dict.
-# Mirela: get the index of the specified nucleotides dinuc1 and dinuc2 in the array dinucs
-    assert len (dinucs) == 11
-    d1 = dinuc1.tostring().upper()
-    d2 = dinuc2.tostring().upper()
-    if ((d1 == 'A' and d2 == 'A') or (d1 == 'T' and d2 == 'T')): return 0
-    if ((d1 == 'A' and d2 == 'C') or (d1 == 'G' and d2 == 'T')): return 1
-    if ((d1 == 'A' and d2 == 'G') or (d1 == 'C' and d2 == 'T')): return 2
-    if  (d1 == 'A' and d2 == 'T'): return 3
-    if ((d1 == 'C' and d2 == 'A') or (d1 == 'T' and d2 == 'G')): return 4
-    if ((d1 == 'C' and d2 == 'C') or (d1 == 'G' and d2 == 'G')): return 5
-    if  (d1 == 'C' and d2 == 'G'): return 6
-    if ((d1 == 'G' and d2 == 'A') or (d1 == 'T' and d2 == 'C')): return 7
-    if  (d1 == 'G' and d2 == 'C'): return 8
-    if  (d1 == 'T' and d2 == 'A'): return 9
-    return 10
-
-
 def update_extrema(func, extrema, data, col_index):
     extrema[col_index] = new_extrema(func, data, extrema[col_index])
-
 
 def write_metadata(chromosome):
     print >>sys.stderr, "writing metadata for %s" % chromosome.title
@@ -64,28 +34,8 @@ def write_metadata(chromosome):
     sums_squares = fill_array(0.0, row_shape)
     num_datapoints = fill_array(0, row_shape)
 
-    # Mirela: add the counts for nucleotides and dinucleotides
-    seq_counts_names = array(['A|T', 'C|G', 'ambig_nucleotide', 'AA|TT', 'AC|GT', 'AG|CT', 'AT', 'CA|TG', 'CC|GG', 'CG', 'GA|TC', 'GC', 'TA', 'ambig_dinucleotide'])
-    seq_counts = fill_array(0, len(seq_counts_names))
-    seq_total_counts = 0        # the total number of nucleotides in this chromosome (only what is in supercontigs)
-
     for supercontig, continuous in walk_continuous_supercontigs(chromosome):
         print >>sys.stderr, " scanning %s" % supercontig._v_name
-
-        # Mirela: compute the nucleotide counts
-        nuc_categories = seq_counts_names[0:3]
-        for nucleotide_int in supercontig.seq.read():
-            nuc_index = get_nucleotide_index(nucleotide_int, nuc_categories)
-            seq_counts[nuc_index] += 1
-
-        # Mirela: compute the dinucleotide counts
-        dinuc_categories = seq_counts_names[3:]
-        for i in range(len(supercontig.seq.read())-1):
-            dinuc_index = get_dinucleotide_index(supercontig.seq.read(i), supercontig.seq.read(i+1), dinuc_categories)
-            seq_counts[len(nuc_categories)+dinuc_index] += 1
-
-        # Mirela: also store the total number of nucleotides in all supercontigs 
-        seq_total_counts += len(supercontig.seq.read())
 
         # only runs when assertions checked
         if __debug__:
@@ -164,11 +114,6 @@ def write_metadata(chromosome):
     chromosome_attrs.sums_squares = sums_squares
     chromosome_attrs.num_datapoints = num_datapoints
     chromosome_attrs.dirty = False
-    chromosome_attrs.seq_counts = seq_counts
-    chromosome_attrs.seq_counts_names = seq_counts_names
-    chromosome_attrs.seq_total_counts = seq_total_counts
-
-    print seq_counts
 
 def save_metadata(*filenames):
     for filename in filenames:
