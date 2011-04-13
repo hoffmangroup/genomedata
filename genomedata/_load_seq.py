c1e5af46a39b0caa4d9b1a848c11764afda1d44a
mmh1 <devnull@localhost>
2011-04-13 03:48:46 +0000

    fix error where genomedata files created in assembly directory

--- genomedata/_load_seq.py
+++ genomedata/_load_seq.py
@@ -249,7 +249,7 @@ def load_seq(gdfilename, filenames, verbose=False, mode=None, seqfile_type="fast
 
                 with maybe_gzip_open(filename) as infile:
                     if seqfile_type == "agp":
-                        name = filename.rpartition(".agp")[0]
+                        name = path(filename).name.rpartition(".agp")[0]
                         chromosome = create_chromosome(genome, name, mode)
                         read_assembly(chromosome, infile)
                     else:
