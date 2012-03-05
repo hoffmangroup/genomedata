464df6b46cf35fd9925c5a43c6674162b3bfa31a
mmh1 <devnull@localhost>
2012-03-05 22:17:14 +0000

    increment release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ space footprint. We have also developed utilities to load data into
 this format.
 """
 
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 
 # Copyright 2008-2011 Michael M. Hoffman <mmh1@washington.edu>
 
