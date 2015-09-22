c829d4c36c70213663e4926696fcb0022e9895f8
Jay Hesselberth <jay.hesselberth@gmail.com>
2015-09-22 17:03:33 -0600

    roll back _info sizes cmd

--- genomedata/_info.py
+++ genomedata/_info.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
-from __future__ import absolute_import, division, print_function
-from future_builtins import ascii, filter, hex, map, oct, zip
 
 """
-_info: DESCRIPTION
+_info: report specific information about a genomedata archive.
 """
 
 __version__ = "$Revision$"
@@ -15,6 +13,11 @@ import sys
 
 from . import Genome
 
+from ._util import die
+
+# XXX: add sizes command that produces tab-delimited file of sizes,
+# compatible with UCSC bigWig tab-delimited specification file, for
+# checking
 
 
 def print_tracknames_continuous(genome):
@@ -27,22 +30,13 @@ def print_contigs(genome):
             print("%s\t%s\t%s" % (chrom.name, contig.start, contig.end))
 
 
-def print_sizes(genome):
-    for chrom in genome:
-        # add 1 because chrom sizes files are 1-based, genomedata are
-        # 0-based
-        print('%s\t%d' % (chrom, chrom.end + 1))
+def _info(cmd, filename):
 
-
-def _info(cmd, gdarchive):
-
-    with Genome(gdarchive) as genome:
+    with Genome(filename) as genome:
         if cmd in ["tracknames_continuous", "tracknames"]:
             print_tracknames_continuous(genome)
         if cmd == "contigs":
             print_contigs(genome)
-        if cmd == 'sizes':
-            print_sizes(genome)
 
 
 def parse_options(args):
@@ -57,7 +51,7 @@ def parse_options(args):
                             prog='genomedata-info',
                             version=__version__)
                             
-    choices = ["tracknames", "tracknames_continuous", "contigs", "sizes"]
+    choices = ["tracknames", "tracknames_continuous", "contigs"]
 
     parser.add_argument("command", choices=choices,
                         help='available commands')
@@ -73,7 +67,6 @@ def main(args=sys.argv[1:]):
     args = parse_options(args)
 
     return _info(args.command, args.gdarchive)
-
 
 if __name__ == "__main__":
     sys.exit(main())
