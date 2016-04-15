32d8a281559902825b772ca77d538b0ffcb08ad8
Eric Roberts <eroberts@uhnresearch.ca>
2016-04-15 10:23:18 -0400

    Fix genomedata-open-data interface and usage specification

--- genomedata/_open_data.py
+++ genomedata/_open_data.py
@@ -17,6 +17,7 @@ from argparse import ArgumentParser
 
 from . import Genome, __version__
 
+
 def open_data(gdarchive, tracknames, verbose):
     warnings.simplefilter("ignore")
     with Genome(gdarchive, "r+") as genome:
@@ -26,18 +27,24 @@ def open_data(gdarchive, tracknames, verbose):
 
     warnings.resetwarnings()
 
+
 def parse_options(args):
 
+    # TODO: Change usage string to have archive before the tracknames
     description = ("Open one or more tracks in"
                    " the specified Genomedata archive.")
 
+    usage = "%(prog)s [-h] [-v] [--verbose] gdarchive --tracknames TRACKNAMES "
+    "[TRACKNAMES ...]"
+
     parser = ArgumentParser(description=description,
+                            usage=usage,
                             prog='genomedata-open-data',
                             version=__version__)
 
     parser.add_argument('gdarchive', help='genomedata archive')
 
-    parser.add_argument("--trackname", required=True, nargs='+', 
+    parser.add_argument("--tracknames", required=True, nargs='+',
                         help="tracknames to open")
 
     parser.add_argument("--verbose", default=False, action="store_true",
@@ -46,6 +53,7 @@ def parse_options(args):
     args = parser.parse_args(args)
 
     return args
+
 
 def main(argv=sys.argv[1:]):
     args = parse_options(argv)
