6a6f89e43cdabe168f93a20c972eb5f771473c17
Eric Roberts <eroberts@uhnresearch.ca>
2017-04-19 15:21:13 -0400

    Remove unused peek_line function

--- genomedata/_load_seq.py
+++ genomedata/_load_seq.py
@@ -20,7 +20,7 @@ from tabdelim import DictReader
 
 from . import (SEQ_ATOM, SEQ_DTYPE, FILE_MODE_CHROMS, 
               FORMAT_VERSION, Genome, __version__)
-from ._util import FILTERS_GZIP, LightIterator, maybe_gzip_open, peek_line
+from ._util import FILTERS_GZIP, LightIterator, maybe_gzip_open
 
 MIN_GAP_LEN = 100000
 assert not MIN_GAP_LEN % 2 # must be even for division
