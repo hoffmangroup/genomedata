dee3eceaa1c988bd2264e8090dcf8fd3153403f1
Eric Roberts <eroberts@uhnresearch.ca>
2017-04-25 15:25:25 -0400

    Initial implementation of track filtering for load_data

--- genomedata/_load_data.py
+++ genomedata/_load_data.py
@@ -6,24 +6,23 @@ from __future__ import absolute_import, division, print_function
 _load_data.py: A python interface for genome_load_data.c
 """
 
-import struct
 import sys
 
 from argparse import ArgumentParser
+from pybedtools import cleanup
 from subprocess import PIPE, Popen
 
 from . import __version__
-from ._util import SUFFIX_GZ, die
+from ._util import SUFFIX_GZ, die, get_bed_from_track_file, score_filter
 
-BIG_WIG_SIGNATURE = 0x888FFC26
-BIG_WIG_SIGNATURE_BYTE_SIZE = 4
 BIG_WIG_READ_CMD = "bigWigToBedGraph"
 DEFAULT_CHUNK_SIZE = 10000
 LOAD_DATA_CMD = "genomedata-load-data"
 MSG_LOAD_ERROR = "Error loading data from track file %%s. %s returned %%s." % LOAD_DATA_CMD
 
 
-def load_data(gdfilename, trackname, datafile, verbose=False):
+def load_data(gdfilename, trackname, datafile, filterfile=None,
+              filter_threshold=None, verbose=False):
     """Loads data from datafile into specific track of Genomedata archive
 
     gdfilename: genomedata archive path
@@ -33,20 +32,47 @@ def load_data(gdfilename, trackname, datafile, verbose=False):
     if verbose:
         print(">> Loading data for track: %s" % trackname)
 
-    file_is_big_wig = is_big_wig(datafile)
+    # Check wigFix
+    # Get BED from genomic file
+    input_bed = get_bed_from_track_file(datafile)
+    # If there is a filter being appled
+    if filterfile:
+        if verbose:
+            print(">> Filtering ", trackname, " with ", filterfile)
+        # Get BED from filter input file
+        filter_bed = get_bed_from_track_file(filterfile)
 
-    if file_is_big_wig:
-        read_cmd = [BIG_WIG_READ_CMD]
-    elif datafile.endswith(SUFFIX_GZ):
-        read_cmd = ["zcat"]
+        # If there is a threshold given
+        if filter_threshold:
+            # Filter the filter by the threshold given
+            filter_bed = filter_bed.filter(score_filter,
+                                           threshold=filter_threshold)
+
+        # Interesect the filter with the input data track
+        result_bed = input_bed.intersect(filter_bed)
+        # Merge results
+        result_bed = result_bed.merge()
+    # Otherwise just use the input genomic file
     else:
-        read_cmd = ["cat"]  # XXX: useless use of cat
-    read_cmd.append(datafile)
+        result_bed = input_bed
 
-    # If the file is a BigWig
-    if file_is_big_wig:
-        # set bigWigToBedGraph to output to stdout
-        read_cmd.append("/dev/stdout")
+    if verbose:
+        print(">> Using processed track data from", result_bed.fn)
+
+    # file_is_big_wig = is_big_wig(datafile)
+
+    # if file_is_big_wig:
+    #     read_cmd = [BIG_WIG_READ_CMD]
+    # elif datafile.endswith(SUFFIX_GZ):
+    #     read_cmd = ["zcat"]
+    # else:
+    #     read_cmd = ["cat"]  # XXX: useless use of cat
+    # read_cmd.append(datafile)
+    #
+    # # If the file is a BigWig
+    # if file_is_big_wig:
+    #     # set bigWigToBedGraph to output to stdout
+    #     read_cmd.append("/dev/stdout")
 
     load_cmd = [LOAD_DATA_CMD]
     if verbose:
@@ -54,51 +80,19 @@ def load_data(gdfilename, trackname, datafile, verbose=False):
     load_cmd.extend([gdfilename, trackname])
 
     if verbose:
-        read_cmdline = " ".join(read_cmd)
+        # read_cmdline = " ".join(read_cmd)
         load_cmdline = " ".join(load_cmd)
-        print(read_cmdline, load_cmdline, sep=" | ",  file=sys.stderr)
+        print(load_cmdline, result_bed.fn, sep=" < ",  file=sys.stderr)
 
-    # Pipe read command into load command
-    try:
-        reader = Popen(read_cmd, stdout=PIPE)
-    except OSError as os_exception:
-        # If it was a big wig file and the converting program was not found on
-        # the path
-        if file_is_big_wig:
-            # Report that the program could not be found
-            raise OSError("A BigWig file was detected but " +
-                          BIG_WIG_READ_CMD +
-                          " could not be found on your path to convert it for "
-                          "use in Genomedata. Please install the utility "
-                          "from UCSC and try again.")
-        # Otherwise if there was another exception where the file isn't BigWig
-        else:
-            # Re-raise the exception
-            raise os_exception
+    with open(result_bed.fn) as input_track_bed_file:
+        loader = Popen(load_cmd, stdin=input_track_bed_file)
+        retcode = loader.wait()
 
-    loader = Popen(load_cmd, stdin=reader.stdout)
-    loader.communicate()
-    retcode = loader.poll()
     if retcode != 0:
         die(MSG_LOAD_ERROR % (datafile, retcode))
 
-
-def is_big_wig(filename):
-    """ Checks that the given filename refers to a valid bigWig file """
-    with open(filename, "rb") as big_wig_file:
-        signature_string = big_wig_file.read(BIG_WIG_SIGNATURE_BYTE_SIZE)
-
-    # unpack returns a tuple regardless of length
-    # the kent reference checks both little endian and big endian packing
-    # of the 4 byte signature
-    little_endian_signature = struct.unpack("<L", signature_string)[0]
-    big_endian_signature = struct.unpack(">L", signature_string)[0]
-
-    if (little_endian_signature == BIG_WIG_SIGNATURE or
-       big_endian_signature == BIG_WIG_SIGNATURE):
-        return True
-
-    return False
+    # Force clean up of temporary files only made in this function
+    cleanup()
 
 
 def parse_args(args):
--- genomedata/_util.py
+++ genomedata/_util.py
@@ -7,10 +7,17 @@ from __future__ import absolute_import, division, print_function
 from contextlib import closing
 from gzip import open as _gzip_open
 from os import extsep
+from pybedtools import BedTool
+from pybedtools.contrib.bigwig import bigwig_to_bedgraph
+import struct
 import sys
 
 from numpy import array, empty
 from tables import Filters
+
+BIG_WIG_FIELD_COUNT = 4
+BIG_WIG_SIGNATURE = 0x888FFC26
+BIG_WIG_SIGNATURE_BYTE_SIZE = 4
 
 FILTERS_GZIP = Filters(complevel=1)
 
@@ -88,6 +95,70 @@ def new_extrema(func, data, extrema):
     curr_extrema = func(data, 0)
 
     return func([extrema, curr_extrema], 0)
+
+
+def get_float_from_filter_score(score):
+    try:
+        result = float(score)
+    except ValueError:
+        print("Found ", score, "in filter value, could not convert to a "
+              "number")
+        raise
+    return result
+        
+
+def score_filter(bed_interval, threshold):
+    """ Returns true or false if the given  BED interval object
+    (pybedtools.Interval) if the value assigned to that interval is greater
+    or equal to the threshold given. Attempts to work with both a properly
+    formatted bigWig and BED file """
+    # NB: a bigwig file will have the "score" in the 4th field while a
+    # regular bed file will have it's score in it's normal 5th field
+
+    bed_interval_field_count = len(bed_interval.fields)
+    # If there are only 4 fields (bigWig or a bedGraph)
+    # Attempt to only use the 4th field for the score to compare against
+    if bed_interval_field_count == BIG_WIG_FIELD_COUNT:
+        score = get_float_from_filter_score(bed_interval[BIG_WIG_FIELD_COUNT-1])
+    # If there are more fields than a bedGraph, assume a bed format
+    elif bed_interval_field_count > BIG_WIG_FIELD_COUNT:
+        score = get_float_from_filter_score(bed_interval.score)
+    # Otherwise if there are too few fields
+    else:
+        # Raise an error with an appropriate message
+        raise ValueError("No value for a threshold found in filter file")
+
+    return score >= threshold
+
+
+def is_big_wig(filename):
+    """ Checks that the given filename refers to a valid bigWig file """
+    with open(filename, "rb") as big_wig_file:
+        signature_string = big_wig_file.read(BIG_WIG_SIGNATURE_BYTE_SIZE)
+
+    # unpack returns a tuple regardless of length
+    # the kent reference checks both little endian and big endian packing
+    # of the 4 byte signature
+    little_endian_signature = struct.unpack("<L", signature_string)[0]
+    big_endian_signature = struct.unpack(">L", signature_string)[0]
+
+    if (little_endian_signature == BIG_WIG_SIGNATURE or
+       big_endian_signature == BIG_WIG_SIGNATURE):
+        return True
+
+    return False
+
+
+def get_bed_from_track_file(filename):
+    """ Returns a pybedtools.BedTool object from a given filename. Attempts to
+    automatically detect the bigWig format and convert"""
+    # If the file is a bigWig
+    if is_big_wig(filename):
+        return bigwig_to_bedgraph(filename)
+    # Otherwise read in normal bed file
+    else:
+        return BedTool(filename)
+
 
 def main(args=sys.argv[1:]):
     pass
